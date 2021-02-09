00ed27eb5aece873e5753f12ea67344fdaee47d8
алина человечкова <alinasmart19@mail.ru>
2021-02-09 16:04:47 +0600

    Добавьте файлы проекта.

---
+++ ButtonDelegate/Button.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+namespace ButtonDelegate
+{
+    public delegate void ButtonActionDelegate(params object[] array);
+
+    public class Button
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Text { get; set; }
+        public string Color { get; set; }
+        public ConsoleColor hBorderColor;
+
+        public event Action<string> ButtonAction;
+
+        public void Draw()
+        {
+            string s = "╔";
+            string space = "";
+            string temp = "";
+            for (int i = 0; i < Width; i++)
+            {
+                space += " ";
+                s += "═";
+            }
+            s += "╗" + "\n";
+            string space2 = space;
+            for (int i = 0; i < Height; i++)
+            {
+                if (i == 1)
+                {
+                    s += temp + "║" + Text + space2.Remove(0, Text.Length) + "║" + "\n";
+                }
+                else
+                    s += temp + "║" + space + "║" + "\n";
+            }
+
+            s += temp + "╚";
+            for (int i = 0; i < Width; i++)
+                s += "═";
+
+            s += "╝" + "\n";
+
+            Console.ForegroundColor = hBorderColor;
+            ButtonAction?.Invoke(s);
+        }
+    }
+}
---
+++ ButtonDelegate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ButtonDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var button = new Button
+            {
+                Width = 9,
+                Height = 3,
+                Text = "Click",
+                hBorderColor = ConsoleColor.Red,
+            };
+
+            button.ButtonAction += Console.WriteLine;
+            button.ButtonAction += data => Save(data);
+            button.Draw();
+        }
+
+        private static void Save(string data)
+        {
+
+        }
+    }
+}
